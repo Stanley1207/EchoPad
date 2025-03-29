e4151347e17b6ed4064275c4cfcb20dd05ca592d
Stanley Zhang <stanleyzhang1207@gmail.com>
2025-03-29 15:27:45 -0400

    please grade homework9 osx

---
+++ ParticleSystem.cpp
@@ -0,0 +1,8 @@
+//
+//  ParticleSystem.cpp
+//  game_engine
+//
+//  Created by Stanley  on 3/28/25.
+//
+
+#include "ParticleSystem.hpp"
---
+++ ParticleSystem.hpp
@@ -0,0 +1,29 @@
+//
+//  ParticleSystem.hpp
+//  game_engine
+//
+//  Created by Stanley  on 3/28/25.
+//
+
+#ifndef ParticleSystem_hpp
+#define ParticleSystem_hpp
+
+#include <string>
+#include <vector>
+#include <queue>
+#include "Helper.h"
+#include "Renderer.hpp"
+#include "lua/lua.hpp"
+#include "LuaBridge/LuaBridge.h"
+
+class Actor;
+
+struct Particle{
+    
+};
+
+struct ParticleSystem{
+    
+};
+
+#endif /* ParticleSystem_hpp */
